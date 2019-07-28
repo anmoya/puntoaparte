7dcead5cd2f5072b6be092032a3e311c4bb62ff3
anmoya <anmoya@uc.cl>
2019-07-28 11:21:08 -0400

    First commit spa

---
+++ puntoaparte.spa/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <TheNavBar />
+    <BaseGrid :books="books" />
+  </div>
+</template>
+
+<script>
+import TheNavBar from './components/TheNavBar'
+import BaseGrid from './components/BaseGrid'
+import axios from 'axios'
+
+
+
+export default {
+  name: 'app',
+  components: {
+    TheNavBar,
+    BaseGrid
+  },
+  data () {
+    return {
+      books: []
+    }
+  },
+  mounted: function() {
+    axios.get('https://api.nytimes.com/svc/books/v3/lists.json?list=hardcover-fiction&api-key=blAqKba8iuT9CvGIw4G0JFVjKdpjlj3D')
+      .then(response => {
+        //console.log(response)
+        this.books = response.data.results
+      })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ puntoaparte.spa/src/components/BaseLanding.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Nav</h1>
+        <h1>Body</h1>
+        <h1>Foot</h1>
+    </div>
+</template>
+
+
